0b810adb11174aa0e8fce4f82d8ad8de109719ab
Ivan <ivan.jaimes.y90m@statefarm.com>
2020-07-16 22:36:38 -0500

    Init commit. Setup folder structure. USE A LINTER PLZ.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Dashboard from "./Pages/Dashboard";
+import { makeStyles } from "@material-ui/core/styles";
 
+// CSS Should be done this way
+const useStyles = makeStyles({
+  root: {
+    background: "#FBFCFD",
+    minHeight: "100vh",
+  },
+});
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {/* TODO Add react routes here */}
+      <Dashboard />
     </div>
   );
 }
---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+// CSS Should be done this way
+const useStyles = makeStyles({
+  root: {
+    background: "#FBFCFD",
+  },
+});
+
+export default function Dashboard() {
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <Typography variant="h2">Dashboard</Typography>
+    </Container>
+  );
+}
