b428a8be68d23f5a024e79374d45391134ed7545
Ivan <ivan.jaimes.y90m@statefarm.com>
2020-09-06 15:58:56 -0500

    added environmental variables

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target:
-        "http://ec2co-ecsel-w7q6c99ot638-2098345273.us-east-1.elb.amazonaws.com:5000/",
+      target: process.env.BACKEND_URL,
       changeOrigin: true,
     })
   );
