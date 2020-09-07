c47cd60c70e8529b394cef2930e2ef90ee7208a9
MickPoletti <mmcdhunter1@yahoo.com>
2020-09-06 22:09:45 -0500

    Push for now
    just a quick push for now

---
+++ src/Components/clientCard/ClientCard.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Container, Typography} from "@material-ui/core";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/core/styles";
+import ContactTile from '../contactTile/ContactTile';
+// CSS Should be done this way
+const useStyles = makeStyles({
+  root: ({BackgroundColor, TextColor}) => ( {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '23.12%',
+    textAlign: 'left',
+    /* White / High Emphasis */
+    background: BackgroundColor,
+    /* Gray / Divider */
+    border: '1px solid #E5E5E5',
+    marginLeft: '3%',
+    marginTop: '8em',
+    paddingBottom: '2%',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '20px',
+    lineheight: '30px',
+    /* identical to box height, or 150% */
+    letterSpacing: '0.15px',
+    color: TextColor
+  }),
+  upcomingClientsCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '23.12%',
+    textAlign: 'left',
+    /* White / High Emphasis */
+    background: '#FFFFFF',
+    /* Gray / Divider */
+    border: '1px solid #E5E5E5',
+    marginLeft: '3%',
+    marginTop: '2em',
+    paddingBottom: '2%',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '20px',
+    lineheight: '30px',
+    /* identical to box height, or 150% */
+    letterSpacing: '0.15px',
+    color: 'rgba(0, 0, 0, 0.87)'
+  },
+});
+
+
+export default function ClientCard({CardTitle, BackgroundColor, TextColor, PatientName, PhoneNumber, AppointmentStartTime})
+{ 
+  const classes = useStyles({BackgroundColor, TextColor});
+  return (
+  <Card className={classes.root}>
+    <Typography variant="h5" style={{paddingTop: '30px', paddingLeft:'20px'}}>{CardTitle}</Typography>
+    <CardContent>
+        <ContactTile  PatientName={PatientName} PhoneNumber={PhoneNumber} AppointmentStartTime={AppointmentStartTime}/>
+    </CardContent>
+  </Card>);
+}
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -3,9 +3,17 @@ import Button from "@material-ui/core/Button";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import DashBoardAlertsCard from "../Components/dashboardAlertsCard/DashBoardAlertsCard"
+import ClientCard from "../Components/clientCard/ClientCard"
+import PeopleIcon from '@material-ui/icons/People';
+import AlertIcon from '@material-ui/icons/Report';
 
 const useStyles = makeStyles({
   root: {},
+  row: {
+    display: 'flex',
+    flexDirection: 'row'
+  }
 });
 
 export default function Dashboard() {
@@ -14,16 +22,15 @@ export default function Dashboard() {
     <div className={classes.root}>
       <Typography variant="h4" gutterBottom>
         Dashboard
-      </Typography>
-      <Typography variant="h6"> Content</Typography>
+      </Typography> 
+      <div className={classes.row}>
+        <DashBoardAlertsCard cardName="Patients" amount="8" icon={PeopleIcon} />
+        <DashBoardAlertsCard cardName="Reports" amount="4" icon={AlertIcon} />
+      </div>
+      <ClientCard CardTitle="Current Client" BackgroundColor="#2196F3" TextColor="#FFFFFF" PatientName="Nicci Troiani" PhoneNumber="(555)-555-5555" AppointmentStartTime="2:45 PM"/>
+      <ClientCard CardTitle="Upcoming" BackgroundColor="#FFFFFF" TextColor="rgba(0, 0, 0, 0.87)" PatientName="Nicci Troiani" PhoneNumber="(555)-555-5555" AppointmentStartTime="2:45 PM"/>
     </div>
-    // <Button className={classes.root}>
-    //   <div style={{marginRight:"30px"}} >
-    //     <DashboardIcon/>
-    //   </div>
-    //   <div style={{marginRight:"97px"}}>
-    //     Dashboard
-    //   </div>
-    // </Button>
   );
 }
+
+
